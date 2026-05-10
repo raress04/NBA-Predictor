f7e5783b715234988c36e6753894282f190011af
Rares Balcan <balcanrares@gmail.com>
2026-05-10 16:37:45 +0300

    chore: cleanup temp refactoring scripts and pycache

--- advanced_model/_deprecated/root/verify_fm.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_parquet('advanced_model/database/feature_matrix.parquet')
-print(df['game_date'].min(), df['game_date'].max())
-print(df['category'].value_counts())
-print(df.columns.tolist())
